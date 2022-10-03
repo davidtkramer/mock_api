0cf87f2e6a409840255c2a87ee6082056e80ce27
David Kramer <david.kramer@redtailtechnology.com>
2022-10-03 08:29:49 -0700

    wip

--- lib/mock_api/railtie.rb
+++ lib/mock_api/railtie.rb
@@ -1,8 +1,9 @@
 module MockApi
   class Railtie < Rails::Railtie
     initializer "my_railtie.configure_rails_initialization" do |app|
-      puts 'RUNNING INITIALIZER'
-      puts app.config.autoload_paths.inspect
+      # app.config.autoload_paths << "#{root}/test/api_mocks"
+      puts 'RUNNING INIT'
+      puts "#{root}/test/api_mocks"
     end
   end
 end
