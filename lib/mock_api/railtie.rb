5225af368c6f9bd3da5e8556525ea3370c117f1a
David Kramer <david.kramer@redtailtechnology.com>
2022-10-04 11:47:38 -0700

    handle mocks in spec folder

--- lib/mock_api/railtie.rb
+++ lib/mock_api/railtie.rb
@@ -1,7 +1,10 @@
 module MockApi
   class Railtie < Rails::Railtie
     initializer "mock_api.add_mocks_to_autoload_paths", before: :set_autoload_paths do |app|
-      app.config.autoload_paths << "#{Rails.root}/test/api_mocks"
+      paths = ["#{Rails.root}/test/api_mocks", "#{Rails.root}/spec/api_mocks"]
+      paths
+        .select { |path| Dir.exist?(path) }
+        .each { |path| app.config.autoload_paths << path }
     end
   end
 end
